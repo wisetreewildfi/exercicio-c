2587286241516a56d8772cae52a0a04e200c941c
Leandro Bitencourt <leandrocintrabitencourt@gmail.com>
2017-10-27 10:48:05 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 int main (int argc, char** argv)
 {
+    int num; // how you declare your variable int
     printf(“Hello World!\n”); // Trouxemos ele do stdio.h
+    printf(“write number!\n”);
+    scanf("%d",&num);
     return (0);
 }
